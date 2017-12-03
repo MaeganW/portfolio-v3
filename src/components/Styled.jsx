676072592549eb22351dd75b8ced6ce7858f3b22
Maegan Womble <maeganwomble@gmail.com>
2017-12-02 19:36:17 -0600

    testing new scss files for new components

---
+++ src/components/Styled.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/css/styled.css'
+
+const Styled = () => {
+    return (
+        <div className="green">
+            This is a styled component.
+        </div>
+    );
+};
+
+export default Styled;
