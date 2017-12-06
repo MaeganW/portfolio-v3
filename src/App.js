08b26b0b136049ea24bdc8e68db27f3d3ce58e5c
Maegan Womble <maeganwomble@gmail.com>
2017-12-05 23:14:12 -0600

    added react router and main components boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Nav from './components/Common/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import MyWork from './components/MyWork';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Start
-      </div>
+      <Router>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/skills" component={Skills}/>
+          <Route path="/mywork" component={MyWork}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/MyWork.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyWork extends Component {
+  render() {
+    return (
+      <div>
+        my work
+      </div>
+    );
+  }
+}
+
+export default MyWork;
